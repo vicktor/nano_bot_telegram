68e957575f735d65457f90c10c2de622a9c93a90
Victor Bautista <victor.bautista@gmail.com>
2021-03-10 19:12:27 +0100

    Create bot_nano.py

---
+++ bot_nano.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import requests
+import socket
+import json
+
+headers = {
+  'Content-Type': 'application/json'
+}
+
+apikey = "put-your-coinmarketcap-apikey"
+url_nano = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=1567&convert=EUR&CMC_PRO_API_KEY={apikey}".format(apikey=apikey)
+
+response = requests.request("GET", url_nano, headers=headers)
+
+data = response.json()['data']['1567']['quote']['EUR']
+
+price = data['price']
+
+nanos = 249
+
+amount = nanos * price
+
+r = requests.post('https://api.telegram.org/bot1560214598:AAHXHQG6goe4YvHGb-xgW3595h1qnqn9m30/sendMessage',
+              data={'chat_id': '3281783', 'text': 'Current quote: {price}, current amount: {amount}'.format(price=round(price,5), amount=round(amount,2))})
